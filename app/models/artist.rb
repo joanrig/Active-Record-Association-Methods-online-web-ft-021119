a37bc366e6beb817cbe28407808c84c63460c93d
Joan Lyness <joanrigdon@gmail.com>
2019-03-26 04:18:31 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -11,6 +11,6 @@ class Artist < ActiveRecord::Base
   end
 
   def genre_count
-    self.songs.genres.count#return the number of genres associated with the artist
+    self.songs.genre.count#return the number of genres associated with the artist
   end
 end
